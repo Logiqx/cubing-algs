8a404382d4043c0b40fc669a664f9f26d9aee97f
Logiqx <software@mikeg.me.uk>
2016-10-23 11:59:26 +0100

    Complete 2L-2GLL

--- data/2l-2gll.js
+++ data/2l-2gll.js
@@ -3,8 +3,8 @@ var algSet = {
   {
     "id": "2L-2GLL",
     "name": "2-Generator Last Layer",
-    "wiki": "https://www.speedsolving.com/wiki/index.php/2GLL",
-    "algdb": "",
+    "wiki": "https://www.speedsolving.com/wiki/index.php/",
+    "algdb": "http://algdb.net/Set/",
     "uses":
     [
       {
@@ -23,8 +23,8 @@ var algSet = {
       "id": "AS",
       "name": "Anti-Sune",
       "wiki": "",
-      "algdb": "COLL AS",
-      "image": "OCLL-AS.png",
+      "algdb": "COLL AS/B1",
+      "image": "OCLL-AS",
       "prob": "4/27",
       "algs" :
       [
@@ -64,8 +64,8 @@ var algSet = {
       "id": "S",
       "name": "Sune",
       "wiki": "",
-      "algdb": "COLL S",
-      "image": "OCLL-S.png",
+      "algdb": "COLL S/C1",
+      "image": "OCLL-S",
       "prob": "4/27",
       "algs" :
       [
@@ -87,8 +87,8 @@ var algSet = {
       "id": "L",
       "name": "L / Bowtie",
       "wiki": "",
-      "algdb": "COLL L",
-      "image": "OCLL-L.png",
+      "algdb": "COLL L/D1",
+      "image": "OCLL-L",
       "prob": "4/27",
       "algs" :
       [
@@ -128,8 +128,8 @@ var algSet = {
       "id": "U",
       "name": "U / Headlights",
       "wiki": "",
-      "algdb": "COLL U",
-      "image": "OCLL-U.png",
+      "algdb": "COLL U/E1",
+      "image": "OCLL-U",
       "prob": "4/27",
       "algs" :
       [
@@ -181,8 +181,8 @@ var algSet = {
       "id": "T",
       "name": "T / Chameleon",
       "wiki": "",
-      "algdb": "COLL T",
-      "image": "OCLL-T.png",
+      "algdb": "COLL T/F1",
+      "image": "OCLL-T",
       "prob": "4/27",
       "algs" :
       [
@@ -234,8 +234,8 @@ var algSet = {
       "id": "Pi",
       "name": "Pi / Bruno",
       "wiki": "",
-      "algdb": "COLL PI",
-      "image": "OCLL-Pi.png",
+      "algdb": "COLL PI/G1",
+      "image": "OCLL-Pi",
       "prob": "4/27",
       "algs" :
       [
@@ -266,8 +266,8 @@ var algSet = {
       "id": "H",
       "name": "H / Double Sune",
       "wiki": "",
-      "algdb": "COLL H",
-      "image": "OCLL-H.png",
+      "algdb": "COLL H/H1",
+      "image": "OCLL-H",
       "prob": "2/27",
       "algs" :
       [
@@ -316,16 +316,281 @@ var algSet = {
       ]
     },
     {
-      "id": "-",
+      "id": "OCLL",
       "name": "Corners Oriented",
       "wiki": "",
-      "algdb": "",
-      "image": "OCLL.png",
+      "algdb": "..",
+      "image": "OCLL",
       "prob": "1/27",
       "algs" :
       [
         {
-          "alg": "N/A",
+          "alg": "Yay!",
+          "desc": "",
+		  "status": 1,
+          "uses": ["OH", "2H"]
+        },
+      ]
+    },
+    {
+      "id": "Ua",
+      "name": "U-Perm",
+      "desc": "Inverse and reflection of Ub.",
+      "wiki": "",
+      "algdb": "PLL/Ua",
+      "image": "EPLL-Ua",
+      "prob": "1/3",
+      "algs" :
+      [
+        {
+          "alg": "U M2 U (M U2 M' U2') U' M2'",
+          "desc": "[MU] [commutator] / [conjugate] -  [M2 U: [M, U2]]",
+		  "status": 1,
+          "uses": [],
+		  "vars":
+		  [
+			{
+			  "alg": "U M2' U M U2' M' U M2'",
+			  "desc": "Two-handed execution applies a cancellation",
+			  "status": 1,
+			  "uses": ["2H"],
+			},
+		  ]
+        },
+        {
+          "alg": "U' M2 U (M' U2 M U2') U' M2'",
+          "desc": "[MU] [commutator] / [conjugate] -  [M2 U: [M', U2]]",
+		  "status": 1,
+          "uses": [],
+		  "vars":
+		  [
+			{
+			  "alg": "U' M2' U M' U2' M U M2'",
+			  "desc": "Two-handed execution applies a cancellation",
+			  "status": 1,
+			  "uses": ["2H"],
+			},
+		  ]
+        },
+        {
+          "alg": "U R U' (R U R U R U' R' U' R' U') U R'",
+          "desc": "[Conjugate] of [F2L] insert - [R U': R U R U R U' R' U' R' U']",
+		  "status": 1,
+          "uses": [],
+		  "vars":
+		  [
+			{
+			  "alg": "U R U' R U R U R U' R' U' R2",
+			  "desc": "Two-handed execution applies cancellations",
+			  "status": 0,
+			  "uses": ["2H"],
+			},
+			{
+			  "alg": "U R U' R U R U R U' R' U' R2",
+			  "desc": "One-handed execution applies cancellations",
+			  "status": 1,
+			  "uses": ["OH"],
+			},
+		  ]
+        },
+        {
+          "alg": "U' R' U (U' R' U' R' U' R U R U R) U' R",
+          "desc": "[Conjugate] of [F2L] insert - [R' U: U' R' U' R' U' R U R U R]",
+		  "status": 1,
+          "uses": [],
+		  "vars":
+		  [
+			{
+			  "alg": "U' R2 U' R' U' R U R U R U' R",
+			  "desc": "Two-handed execution applies cancellations",
+			  "status": 0,
+			  "uses": ["2H"],
+			},
+			{
+			  "alg": "U' R2 U' R' U' R U R U R U' R",
+			  "desc": "One-handed execution applies cancellations",
+			  "status": 1,
+			  "uses": ["OH"],
+			},
+		  ]
+        },
+      ]
+    },
+    {
+      "id": "Ub",
+      "name": "U-Perm",
+      "desc": "Inverse and reflection of Ua.",
+      "wiki": "",
+      "algdb": "PLL/Ub",
+      "image": "EPLL-Ub",
+      "prob": "1/3",
+      "algs" :
+      [
+        {
+          "alg": "U M2 U' (M U2 M' U2') U M2'",
+          "desc": "[MU] [commutator] / [conjugate] -  [M2 U': [M, U2]]",
+		  "status": 1,
+          "uses": [],
+		  "vars":
+		  [
+			{
+			  "alg": "U M2' U' M U2' M' U' M2'",
+			  "desc": "Two-handed execution applies a cancellation",
+			  "status": 1,
+			  "uses": ["2H"],
+			},
+		  ]
+        },
+        {
+          "alg": "U' M2 U' (M' U2 M U2') U M2'",
+          "desc": "[MU] [commutator] / [conjugate] -  [M2 U': [M', U2]]",
+		  "status": 1,
+          "uses": [],
+		  "vars":
+		  [
+			{
+			  "alg": "U' M2' U' M' U2' M U' M2'",
+			  "desc": "Two-handed execution applies a cancellation",
+			  "status": 1,
+			  "uses": ["2H"],
+			},
+		  ]
+        },
+        {
+          "alg": "U R U' (U R U R U R' U' R' U' R') U R'",
+          "desc": "[Conjugate] of [F2L] insert - [R U': U R U R U R' U' R' U' R']",
+		  "status": 1,
+          "uses": [],
+		  "vars":
+		  [
+			{
+			  "alg": "U R2 U R U R' U' R' U' R' U R'",
+			  "desc": "Two-handed execution applies cancellations",
+			  "status": 0,
+			  "uses": ["2H"],
+			},
+			{
+			  "alg": "U' z U2' R U R U' R' U' R' U' R U'",
+			  "desc": "One-handed execution is on the left",
+			  "status": 1,
+			  "uses": ["OH"],
+			},
+		  ]
+		},
+        {
+          "alg": "U' R' U (R' U' R' U' R' U R U R U) U' R",
+          "desc": "[Conjugate] of [F2L] insert - [R' U: R' U' R' U' R' U R U R U]",
+		  "status": 1,
+          "uses": [],
+		  "vars":
+		  [
+			{
+			  "alg": "U' R' U R' U' R' U' R' U R U R2",
+			  "desc": "Two-handed execution applies cancellations",
+			  "status": 0,
+			  "uses": ["2H"],
+			},
+			{
+			  "alg": "U z U' R U' R' U' R' U' R U R U2'",
+			  "desc": "One-handed execution is on the left",
+			  "status": 1,
+			  "uses": ["OH"],
+			},
+		  ]
+        },
+      ]
+    },
+    {
+      "id": "Z",
+      "name": "Z-Perm",
+      "wiki": "",
+      "algdb": "PLL/Z",
+      "image": "EPLL-Z",
+      "prob": "1/6",
+      "algs" :
+      [
+        {
+          "alg": "(M' U' M2' U' M2' U' M) (M2 U2' M2')",
+          "desc": "Intuitive - Solve UF + UB then solve remaining edges",
+		  "status": 1,
+          "uses": [],
+		  "vars":
+		  [
+			{
+			  "alg": "M' U' M2' U' M2' U' M' U2' M2'",
+			  "desc": "Two-handed execution applies a cancellation",
+			  "status": 1,
+			  "uses": ["2H"],
+			},
+		  ]
+        },
+        {
+          "alg": "U R' U' R U' R U R U' R' U R U R2 U' R'",
+          "desc": "Playing with [F2L] pairs - track BR + FR",
+		  "status": 1,
+          "uses": ["OH"],
+        },
+        {
+          "alg": "U (M2 U' M2 U') (M' U2' M2 U2' M')",
+          "desc": "Intuitive - Solve UL + UR then solve remaining edges",
+		  "status": -1,
+          "uses": [],
+		  "vars":
+		  [
+			{
+			  "alg": "U M2' U' M2' U' M' U2' M2' U2' M'",
+			  "desc": "Two-handed execution",
+			  "status": -1,
+			  "uses": ["2H"],
+			},
+		  ]
+        },
+      ]
+    },
+    {
+      "id": "HX",
+      "name": "H-Perm / X-Perm",
+      "desc": "Best known as H-Perm (i.e. swapping opposite edge pairs) this case is also known as X-Perm (i.e.  swapping diagonal corner pairs).",
+      "wiki": "",
+      "algdb": "PLL/H",
+      "image": "EPLL-H",
+      "prob": "1/12",
+      "algs" :
+      [
+        {
+          "alg": "M2 U' (M2 U2 M2' U2') U M2'",
+          "desc": "[MU] [commutator] / [conjugate] -  [M2 U': [M2, U2]]",
+		  "status": 1,
+          "uses": [],
+		  "vars":
+		  [
+			{
+			  "alg": "M2' U' M2' U2' M2' U' M2'",
+			  "desc": "Two-handed execution applies a cancellation",
+			  "status": 1,
+			  "uses": ["2H"],
+			},
+		  ]
+        },
+        {
+          "alg": "(R2 U2' R U2' R2) U2' (R2 U2' R U2' R2)",
+          "desc": "Note the Ortega / Varasano [OLL] algorithm (H case)",
+		  "status": 1,
+          "uses": ["OH"],
+        },
+      ]
+    },
+    {
+      "id": "EPLL",
+      "name": "Edges Permuted",
+      "wiki": "",
+      "algdb": "PLL",
+      "image": "EPLL",
+      "prob": "1/12",
+      "algs" :
+      [
+        {
+          "alg": "Yay!",
           "desc": "",
 		  "status": 1,
           "uses": ["OH", "2H"]
@@ -344,7 +609,16 @@ var algSet = {
         {
           "id": "corners",
           "name": "Corner Orientation",
-          "cases": ["AS", "S", "L", "U", "T", "Pi", "H", "-"]
+		  "desc": "These are the 7 [COLL] cases where corner swaps are not required. All of these cases can be solved using [RU] algorithms.",
+          "wiki": "COLL",
+          "cases": ["AS", "S", "L", "U", "T", "Pi", "H", "OCLL"]
+        },
+        {
+          "id": "edges",
+          "name": "Edge Permutation",
+		  "desc": "This group of cases is known as [EPLL]. All of these cases can be solved using [MU] or [RU] algorithms.",
+          "wiki": "EPLL",
+          "cases": ["Ua", "Ub", "Z", "HX", "EPLL"]
         },
       ]
     },
