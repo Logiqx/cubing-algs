7532e5e3df7dcb928384d2748382e939cfac879c
Logiqx <software@mikeg.me.uk>
2016-10-26 22:21:05 +0100

    Move tracking to HTML

--- js/lib.js
+++ js/lib.js
@@ -232,30 +232,6 @@ function initAbbrTouch()
 }
 
 //
-// Remember if tracking has already been enabled
-//
-var tracked = false;
-
-//
-// https://analytics.google.com - This is the Universal Analytics tracking code
-//
-function initTracking()
-{
-	if (tracked == false)
-	{
-		(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-		(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-		m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-		})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
-
-		ga('create', 'UA-86348435-1', 'auto');
-		ga('send', 'pageview');
-
-		tracked = true;
-	}
-}
-
-//
 // Remember the last URL fragment so unnecessary re-rendering can be avoided
 //
 var lastFragment = "n/a";
@@ -295,9 +271,6 @@ function processHash()
 
 		// Initialise abbreviations on touch screen devices
 		initAbbrTouch();
-		
-		// Google Analytics website tracking
-		initTracking();
 	}
 }
 
