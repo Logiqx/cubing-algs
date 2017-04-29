80f79747ba48904ed72e6f08f92df667915e0a5a
Logiqx <software@mikeg.me.uk>
2017-04-29 18:59:29 +0100

    Index: Tweaked title

--- data/index-min-001.js
+++
@@ -1,1 +0,0 @@
-var algSet={header:{id:"Mike",name:"Mike's Cube Algorithms",desc:"This site lists the algorithms that I use during my own speedsolves and when teaching people to solve the Rubik's cube.<br/><br/>Use the menu button at the top left of the page to navigate the site."},cases:[],views:[{id:"contents",name:"Contents"}]};
